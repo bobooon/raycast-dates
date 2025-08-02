89689c03d4951164a5fc8e8800c959e2d8ecb859
Rob <i942tb@ionsnare.com>
2025-08-02 12:51:14 -0400

    Initial commit

---
+++ src/datetime.tsx
@@ -0,0 +1,84 @@
+import { Action, ActionPanel, Color, List } from '@raycast/api'
+import { parseDate } from 'chrono-node'
+import dayjs from 'dayjs'
+import advancedFormatPlugin from 'dayjs/plugin/advancedFormat'
+import relativeTimePlugin from 'dayjs/plugin/relativeTime'
+import timezonePlugin from 'dayjs/plugin/timezone'
+import utcPlugin from 'dayjs/plugin/utc'
+import weekOfYearPlugin from 'dayjs/plugin/weekOfYear'
+import { useState } from 'react'
+
+dayjs.extend(advancedFormatPlugin)
+dayjs.extend(weekOfYearPlugin)
+dayjs.extend(utcPlugin)
+dayjs.extend(timezonePlugin)
+dayjs.extend(relativeTimePlugin)
+
+function handleConversion(input: string, timezone: string) {
+  if (input.match(/^\d+$/))
+    input = new Date(Number.parseInt(input, 10) * 1000).toString()
+
+  const parsedDate = parseDate(input)
+  if (!parsedDate || parsedDate.toString() === 'Invalid Date')
+    return []
+
+  const date = dayjs(parsedDate).tz(timezone)
+  const fromNow = date.fromNow()
+
+  return [
+    { label: 'Unix (s)', value: date.unix() },
+    { label: 'Unix (ms)', value: date.valueOf() },
+    { label: 'Human Readable', value: date.format('MMMM Do, YYYY [at] hh:mm:ss A (zzz)') },
+    { label: 'DateTime', value: date.format('YYYY-MM-DD HH:mm:ss') },
+    { label: 'UTC', value: date.toString() },
+    { label: 'ISO 8601', value: date.toISOString() },
+    { label: 'Week of Year', value: date.format('wo dddd [of] YYYY') },
+    { label: 'In / Ago', value: String(fromNow).charAt(0).toUpperCase() + String(fromNow).slice(1) },
+  ]
+}
+
+export default function Datetime() {
+  const [input, setInput] = useState('now')
+  const [timezone, setTimezone] = useState(Intl.DateTimeFormat().resolvedOptions().timeZone)
+  const [items, setItems] = useState<{ label: string, value: string | number }[]>([])
+
+  const onTimezoneChange = async (value: string) => {
+    setTimezone(value)
+    setItems(handleConversion(input, value))
+  }
+
+  const onSearchTextChange = async (value: string) => {
+    setInput(value)
+    setItems(handleConversion(value, timezone))
+  }
+
+  return (
+    <List
+      searchBarPlaceholder="Date"
+      filtering={false}
+      searchText={input}
+      onSearchTextChange={onSearchTextChange}
+      searchBarAccessory={(
+        <List.Dropdown tooltip="Timezone" onChange={onTimezoneChange} defaultValue={timezone}>
+          {Intl.supportedValuesOf('timeZone').map(zone => (
+            <List.Dropdown.Item key={zone} value={zone} title={zone} />
+          ))}
+        </List.Dropdown>
+      )}
+    >
+      {items.map(item => (
+        <List.Item
+          key={item.value}
+          title={String(item.value)}
+          accessories={[{ tag: { value: item.label, color: Color.SecondaryText } }]}
+          actions={(
+            <ActionPanel>
+              <Action.CopyToClipboard content={item.value} />
+              <Action.Paste content={item.value} />
+            </ActionPanel>
+          )}
+        />
+      ))}
+    </List>
+  )
+}
